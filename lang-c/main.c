688456927ef67aaedbf6552a160e606651f843a7
Oliver Tale-Yazdi <oliver@tasty.limo>
2025-08-27 15:11:32 +0200

    Fix
    Signed-off-by: Oliver Tale-Yazdi <oliver@tasty.limo>

--- lang-c/main.c
+++ lang-c/main.c
@@ -26,25 +26,26 @@ void jb_hook_on_transfer(char** out_ptr, uint64_t* out_len) {
     jb_init(NAME);
 
     printf("Initial Balance: %lu, gas remaining: %lu\n", jb_service_balance(), jb_service_gas_remaining());
-    // Print some chain params
+
+    // Print all chain params. Will produce something like this:
+    //  item_deposit: 10,
+    //  byte_deposit: 1,
+    //  base_deposit: 100,
+    //  core_count: 341,
+    //  ...
+    char buffer[1000];
+    jb_chain_params_t params = jb_chain_params();
+    jb_chain_params_fmt(&params, buffer, sizeof(buffer));
+    printf("Chain Params: '%s'\n", buffer);
+
+    // Print chain entropy
     char entropy[32];
     jb_chain_entropy_32((uint8_t*)entropy);
-    // print hex string
+    printf("Chain Entropy: 0x");
     for (int i = 0; i < 32; i++) {
         printf("%02x", entropy[i]);
     }
     puts("");
-
-    char* buffer = malloc(1000);
-    if (!buffer) {
-        jb_log_error("malloc", "Failed to allocate buffer");
-    }
-
-    memset(buffer, 1000, 1);
-    jb_chain_params_t params = jb_chain_params();
-    jb_chain_params_fmt(&params, buffer, 1000);
-    printf("MyString: '%s'\n", buffer);
-    free(buffer);
 
     printf("Finished, gas remaining: %lu\n", jb_service_gas_remaining());
 }
